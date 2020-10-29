bda5fc1636440447aba388fc051f6ceac4643c43
Makai Post <postmakai@gmail.com>
2020-10-29 09:17:32 -0700

    added string.toProperCase() to .js

--- my_global.js
+++ my_global.js
@@ -1,4 +1,11 @@
-/**Convert the string to an object key.
+/** Convert the string to proper case. */
+String.prototype.toProperCase = function() {
+    return this.replace(/\w\S*/g, txt => {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    })
+}
+
+/** Convert the string to an object key.
  * 
  * @param {string} [separator] A string used to separate one word from the next in the resulting String. If omitted, the words are converted to camel case.
  */
@@ -6,11 +13,7 @@ String.prototype.toKey = function(separator) {
     separator = separator || ''
     var str = this
 
-    if (separator == '') {
-        str = this.replace(/\w\S*/g, txt => {
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-        })
-    }
+    if (separator == '') str = str.toProperCase()
 
     str = str.replace(' ', separator || '').replace(/[^0-9a-zA-Z_]/g, '')
     str = str.charAt(0).toLowerCase() + str.substr(1)
