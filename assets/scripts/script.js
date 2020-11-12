39c4ccf06f441928efc4c5549d9daffec7576d64
Makai Post <postmakai@gmail.com>
2020-11-12 13:51:07 -0800

    updated docs

---
+++ assets/scripts/script.js
@@ -0,0 +1,14 @@
+$('.copyable')
+    .on('click', event => {
+        if (event.target.matches('button')) {
+            var button = $(event.target)
+            var text = button.parent().find('div').text()
+            
+            var $temp = $('<input>')
+            $('body').append($temp)
+            $temp.val(text).select()
+            document.execCommand('copy')
+            $temp.remove()
+        }
+    })
+    .append($('<button>').text('+'))
